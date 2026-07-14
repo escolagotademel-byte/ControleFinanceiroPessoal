d37a09aeb0febda5c5820cf10c7d84f94918ddd4
escolagotademel-byte <escolagotademel@gmail.com>
2026-07-14 12:04:56 -0300

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,8 +1,9 @@
 const SUPABASE_URL = 'https://jzequkwvltgougdidehx.supabase.co';
 
-const SUPABASE_KEY = 'sb_publishable_YD5ZKKtn6H16nLDVbPUJZw_J82U1Sff';
+const SUPABASE_KEY =
+  'sb_publishable_YD5ZKKtn6H16nLDVbPUJZw_J82U1Sff';
 
 const supabaseClient = supabase.createClient(
-    SUPABASE_URL,
-    SUPABASE_KEY
+  SUPABASE_URL,
+  SUPABASE_KEY
 );
